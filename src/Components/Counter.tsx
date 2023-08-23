03c6615dd77dd42382ef26f18c79d61f14b14493
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-22 23:25:54 -0400

    Added Counter Increment Functionality

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css";
 
 const Counter = () => {
-  return <div>Counter</div>;
+  // Counter's initial state is set to 0
+  const [counter, setCounter] = useState(0);
+  const [changeAmount, setChangeAmount] = useState(1);
+
+  // Function is called everytime the increment button is clicked by the user
+  const incrementClick = () => {
+    // Counter's previous state is now incremented by 1
+    setCounter(counter + changeAmount);
+  };
+  return (
+    <div id="wrapper">
+      <div id="count">
+        <label id="countTitle">Count</label>
+        <input disabled id="email" value={counter} />
+        <div id="buttons">
+          <button id="incrementButton" onClick={incrementClick}>
+            Increment
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
